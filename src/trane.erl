0517b919e748f00f978f51db3d87c79117e5e656
mats cronqvist <mats.cronqvist@gmail.com>
2010-06-09 13:20:45 +0200

    speedup, wip

--- src/trane.erl
+++ src/trane.erl
@@ -12,6 +12,7 @@
 -author('mats cronqvist').
 -export([sax/3
          , unit/0,unit/1
+         , file_parse/1
          , wget_parse/1,wget_print/1]).
 
 sax(Str,Fun,Acc) ->
@@ -87,93 +88,100 @@ maybe_unroll(Tag,{Fun,Acc,Stack}) ->
 -define(ev(X), ?ws(X); X==$>; X==$=).
 
 tokenize(Str) when is_integer(hd(Str))-> tokenize(list_to_binary(Str));
-tokenize(Str) when is_binary(Str) -> tokenize(tz(nil,Str));
+tokenize(Str) when is_binary(Str) -> 
+  case ff(text,Str) of
+    {{tag,""},EStr,{text,Txt}} -> tokenize({text,EStr,{text,Txt}});
+    {{text,Str},"",eof} -> [{eof,{text,Str}}]
+  end;
 
-tokenize({TZ,Str}) ->
-  tokenize(tz(TZ,Str));
 tokenize({text,Str,Token}) ->
   case ff(text,Str) of
     {{tag,""},EStr,{text,Txt}} -> 
-      try [Token,{text,Txt}]++tokenize({{tag,""},EStr})
-      catch _:_ -> tokenize({text,<<Txt/binary,"&lt;",EStr/binary>>,Token})
+      try [Token,{text,Txt}]++tokenize(t({tag,""},EStr))
+      catch _:_ -> TB = list_to_binary(Txt),
+                   tokenize({text,<<TB/binary,"&lt;",EStr/binary>>,Token})
       end;
-    {{text,Str},"",eof} -> [Token,{eof,{text,Str}}]
+    {{text,Txt},"",eof} ->
+      [Token,{eof,{text,Txt}}]
   end;
 tokenize({TZ,Str,Token}) ->
   [{Token,{TZ,Str}}].
 
-tz(nil,?m("<",Str))                       -> {{tag,""},ws(Str)};
-tz(nil,?d(_,Str))                         -> {nil,Str};
+t({tag,""},?m("!--",Str))                -> t({comm,""},Str);
+t({tag,""},?m("!",Str))                  -> t({'!',""},ws(Str));
+t({tag,""},?m("?",Str))                  -> t({que,""},ws(Str));
+t({tag,""},?m("/",Str))                  -> t({end_tag,""},ws(Str));
+t({tag,Tag},?D(X,S))when ?ev(X)          -> t({attr,"",{Tag,[]}},ws(S));
+t({tag,Tag},?d(X,Str))when ?ok(X)        -> t({tag,Tag++[X]},Str);
 
-tz({tag,""},?m("!--",Str))                -> {{comm,""},Str};
-tz({tag,""},?m("!",Str))                  -> {{'!',""},ws(Str)};
-tz({tag,""},?m("?",Str))                  -> {{que,""},ws(Str)};
-tz({tag,""},?m("/",Str))                  -> {{end_tag,""},ws(Str)};
-tz({tag,Tag},?D(X,S))when ?ev(X)          -> {{attr,"",{Tag,[]}},ws(S)};
-tz({tag,Tag},?d(X,Str))when ?ok(X)        -> {{tag,Tag++[X]},Str};
+t({'!',DT},?m(">",Str))                  -> {text,Str,{'!',dc(DT)}};
+t({'!',DT},?d(X,Str))                    -> t({'!',DT++[X]},Str);
 
-tz({'!',DT},?m(">",Str))                  -> {text,Str,{'!',dc(DT)}};
-tz({'!',DT},?d(X,Str))                    -> {{'!',DT++[X]},Str};
+t({que,DT},?m("?>",Str))                 -> {text,Str,{'?',dc(DT)}};
+t({que,DT},?d(X,Str))                    -> t({que,DT++[X]},Str);
 
-tz({que,DT},?m("?>",Str))                 -> {text,Str,{'?',dc(DT)}};
-tz({que,DT},?d(X,Str))                    -> {{que,DT++[X]},Str};
+t({comm,Comm},?m("-->",Str))             -> {text,Str,{comment,Comm}};
+t({comm,Comm},?d(X,Str))                 -> t({comm,Comm++[X]},Str);
 
-tz({comm,Comm},?m("-->",Str))         -> {text,Str,{comment,Comm}};
-tz({comm,Comm},?d(X,Str))                 -> {{comm,Comm++[X]},Str};
+t({etag,Tag,Attrs},?m("/>",Str))         -> {text,Str,{sc,Tag,Attrs}};
+t({etag,"script",Attrs},?m(">",Str))     -> {script,Str,{open,"script",Attrs}};
+t({etag,"style",Attrs},?m(">",Str))      -> {style,Str,{open,"style",Attrs}};
+t({etag,Tag,Attrs},?m(">",Str))          -> {text,Str,{open,Tag,Attrs}};
 
-tz({etag,Tag,Attrs},?m("/>",Str))         -> {text,Str,{sc,Tag,Attrs}};
-tz({etag,"script",Attrs},?m(">",Str))     -> {script,Str,{open,"script",Attrs}};
-tz({etag,"style",Attrs},?m(">",Str))      -> {style,Str,{open,"style",Attrs}};
-tz({etag,Tag,Attrs},?m(">",Str))          -> {text,Str,{open,Tag,Attrs}};
+t({end_tag,Tag},?D(X,S))when ?ev(X)      -> t({end_tag,Tag,'>'},ws(S));
+t({end_tag,Tag,'>'},?m(">",Str))         -> {text,Str,{close,dc(Tag)}};
+t({end_tag,Tag},?d(X,Str))               -> t({end_tag,Tag++[X]},Str);
 
-tz({end_tag,Tag},?D(X,S))when ?ev(X)      -> {{end_tag,Tag,'>'},ws(S)};
-tz({end_tag,Tag,'>'},?m(">",Str))         -> {text,Str,{close,dc(Tag)}};
-tz({end_tag,Tag},?d(X,Str))               -> {{end_tag,Tag++[X]},Str};
+t({attr,"",{Tag,As}},?D(X,S))when ?ev(X) -> t({etag,dc(Tag),As},S);
+t({attr,"",{Tag,As}},?M("/>",S))         -> t({etag,dc(Tag),As},S);
+t({attr,A,{T,As}},?D(X,S))when ?ev(X)    -> t({eatt,{dc(A),T,As}},ws(S));
+t({attr,A,{T,As}},?M("/>",S))            -> t({eatt,{dc(A),T,As}},ws(S));
+t({attr,A,TAs},?d(X,Str))when ?ok(X)     -> t({attr,A++[X],TAs},Str);
 
-tz({attr,"",{Tag,As}},?D(X,S))when ?ev(X) -> {{etag,dc(Tag),As},S};
-tz({attr,"",{Tag,As}},?M("/>",S))         -> {{etag,dc(Tag),As},S};
-tz({attr,A,{T,As}},?D(X,S))when ?ev(X)    -> {{eatt,{dc(A),T,As}},ws(S)};
-tz({attr,A,{T,As}},?M("/>",S))            -> {{eatt,{dc(A),T,As}},ws(S)};
-tz({attr,A,TAs},?d(X,Str))when ?ok(X)     -> {{attr,A++[X],TAs},Str};
+t({eatt,ATAs},?m("=",Str))               -> t({val,ATAs},ws(Str));
+t({eatt,{A,T,As}},S)                     -> t({attr,"",{T,As++[{A,""}]}},ws(S));
 
-tz({eatt,ATAs},?m("=",Str))               -> {{val,ATAs},ws(Str)};
-tz({eatt,{A,T,As}},S)                     -> {{attr,"",{T,As++[{A,""}]}},ws(S)};
+t({val,ATAs},?m("'",Str))                -> t({sqval,"",ATAs},Str);%singlequ
+t({val,ATAs},?m("\"",Str))               -> t({dqval,"",ATAs},Str);%doublequ
+t({val,ATAs},Str)                        -> t({uqval,"",ATAs},Str);%unquoted
 
-tz({val,ATAs},?m("'",Str))                -> {{sqval,"",ATAs},Str};%singlequoted
-tz({val,ATAs},?m("\"",Str))               -> {{dqval,"",ATAs},Str};%doublequoted
-tz({val,ATAs},Str)                        -> {{uqval,"",ATAs},Str};%unquoted
+t({sqval,V,{A,T,As}},?m("'",S))          -> t({attr,"",{T,As++[{A,V}]}},ws(S));
+t({sqval,V,ATAs},?d(X,Str))              -> t({sqval,V++[X],ATAs},Str);
 
-tz({sqval,V,{A,T,As}},?m("'",S))          -> {{attr,"",{T,As++[{A,V}]}},ws(S)};
-tz({sqval,V,ATAs},?d(X,Str))              -> {{sqval,V++[X],ATAs},Str};
+t({dqval,V,{A,T,As}},?m("\"",S))         -> t({attr,"",{T,As++[{A,V}]}},ws(S));
+t({dqval,V,ATAs},?d(X,Str))              -> t({dqval,V++[X],ATAs},Str);
 
-tz({dqval,V,{A,T,As}},?m("\"",S))         -> {{attr,"",{T,As++[{A,V}]}},ws(S)};
-tz({dqval,V,ATAs},?d(X,Str))              -> {{dqval,V++[X],ATAs},Str};
+t({uqval,V,{A,T,As}},?D(X,S))when ?ev(X) -> t({attr,"",{T,As++[{A,V}]}},ws(S));
+t({uqval,V,{A,T,As}},?M("/>",S))         -> t({attr,"",{T,As++[{A,V}]}},ws(S));
+t({uqval,V,ATAs},?d(X,Str))              -> t({uqval,V++[X],ATAs},Str);
 
-tz({uqval,V,{A,T,As}},?D(X,S))when ?ev(X) -> {{attr,"",{T,As++[{A,V}]}},ws(S)};
-tz({uqval,V,{A,T,As}},?M("/>",S))         -> {{attr,"",{T,As++[{A,V}]}},ws(S)};
-tz({uqval,V,ATAs},?d(X,Str))              -> {{uqval,V++[X],ATAs},Str};
+t(script,Str)                            -> ff(script,Str);
+t(style,Str)                             -> ff(style,Str);
+t(text,Str)                              -> ff(text,Str);
 
-tz(script,Str)                            -> ff(script,Str);
-tz(style,Str)                             -> ff(style,Str);
-tz(text,Str)                              -> ff(text,Str);
+t(X,"")                                  -> {X,"",eof}.
 
-tz(X,"")                                  -> {X,"",eof}.
+ff(What,Str) -> ff(What,Str,[]).
 
-ff(What,Str) ->
-  case ff(Str,ff(What),[1,2]) of
-    [Scr,Rest] -> {{tag,""},ws(Rest),{text,Scr}};
-    [] -> {{text,Str},"",eof}
-  end.
+ff(What,<<>>,A) -> {{text,lists:reverse(A)},"",eof};
+ff(What,?m("<",Str),A) -> {{tag,""},Str,{text,lists:reverse(A)}};
+ff(What,?d(H,Str),A) -> ff(What,Str,[H|A]).
 
-ff(script)-> "(.*)<(\\s*/\\s*script\\s*>.*)\$";
-ff(style) -> "(.*)<(\\s*/\\s*style\\s*>.*)\$";
-ff(text)  -> "(.*)<(.*)\$".
+% ff(What,Str) ->
+%   case ff(Str,ff(What),[1,2]) of
+%     [Scr,Rest] -> {{tag,""},ws(Rest),{text,Scr}};
+%     [] -> {{text,Str},"",eof}
+%   end.
 
-ff(Str,P,Groups) ->
-  case re:run(Str,P,[{capture,Groups,binary},dotall,caseless,ungreedy]) of
-    {match,Ms} -> Ms;
-    nomatch -> []
-  end.
+% ff(script)-> "(.*)<(\\s*/\\s*script\\s*>.*)\$";
+% ff(style) -> "(.*)<(\\s*/\\s*style\\s*>.*)\$";
+% ff(text)  -> "(.*)<(.*)\$".
+
+% ff(Str,P,Groups) ->
+%   case re:run(Str,P,[{capture,Groups,binary},dotall,caseless,ungreedy]) of
+%     {match,Ms} -> Ms;
+%     nomatch -> []
+%  end.
 
 ws(?d(X,Str)) when ?ws(X) -> ws(Str);
 ws(Str) -> Str.
@@ -186,6 +194,10 @@ wget(Url) ->
   inets:start(),
   {ok, {_Rc, _Hdrs, Body}} = http:request(get, {Url, []}, [], []),
   Body.
+
+file_parse(File) ->
+  {ok,B} = file:read_file(File),
+  sax(B,fun(T,A)-> [T|A] end, []).
 
 wget_parse(Url) ->
   sax(wget(Url),fun(T,A)-> A++[T] end, []).
