bef909f19f4059284fb3cce73390a8d555e49609
mats cronqvist <masse@klarna.com>
2015-03-27 12:38:39 +0100

    rebarify

--- ebin/trane.app
+++
@@ -1,6 +0,0 @@
-{application,trane,
-             [{description,"SAX-style parser for broken HTML"},
-              {vsn,"0.20"},
-              {registered,[]},
-              {applications,[kernel,stdlib]},
-              {modules,[trane]}]}.
---
+++ src/trane_app.erl
@@ -0,0 +1,16 @@
+-module(trane_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    trane_sup:start_link().
+
+stop(_State) ->
+    ok.
