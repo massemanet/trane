58830ad0cd805d1769725831faa065bd957495b0
mats cronqvist <masse@cronqvi.st>
2018-09-25 15:24:08 +0000

    regexping - mostly works

--- src/trane.erl
+++ src/trane.erl
@@ -111,24 +111,39 @@ maybe_unroll(Tag, State = #state{stack=Stack}) ->
       throw(close_without_open)
   end.
 
--define(DQ, "(?:\"(?:[^\\\"]|\\\")*\")"). %% double quoted string
--define(SQ, "(?:'(?:[^\']|\')*')").       %% single quoted string
--define(UQ, "(?:\\w+)").                  %% unquoted string
+-define(DQ, "(?:\"(?:[^\\\"]|\\\")*\")").  %% double quoted string
+-define(SQ, "(?:'(?:[^\']|\')*')").        %% single quoted string
+-define(UQ, "(?:[^>\\\"\'\\s/]|/(?!>))+"). %% unquoted string
 
 -define(COMMENT,
-        <<"<!--(.*)-->">>).  %% comment content (mandatory, maybe empty)
+        "<!--").            %% comment. closes with "-->"
+-define(COMMENT_END,
+        "-->").             %% comment. closes with "-->"
+-define(DOCTYPE,
+        "<!doctype").       %% the tag can contain strings
+-define(DOCTYPE_END,
+        ">").               %% the tag can contain strings
+-define(XML,
+        "<?xml").           %% the tag can contain attrs. closes with "?>"
+-define(XML_END,
+        "?>").              %% the tag can contain attrs. closes with "?>"
 -define(TAG_START,
-        <<"<(/)?\\s*(",?UQ,")">>).  %% closing tag marker (opt), tag name
+        "<(/)?\\s*(\\w+)"). %% closing tag marker (opt), tag name
 -define(TAG_ATTR,
-        <<"\\G\\s+(",?UQ,")",  %% attr name (mandatory)
+        <<"\\G\\s+(\\w+)",  %% attr name (mandatory)
           "(?:\\s*=\\s*(",?SQ,"|",?DQ,"|",?UQ,"))?">>). %% attr value (opt)
 -define(TAG_END,
-        <<"\\G\s*(\/)?>">>).  %% self-closing tag marker (opt)
+        <<"\\G\s*(\/)?>">>).    %% self-closing tag marker (opt)
+-define(TAG_BEGIN,
+        <<?COMMENT,"|",?DOCTYPE,"|",?XML,"|",?TAG_START>>).
 
 mk_regexps() ->
-  fun(tag_start) -> element(2, re:compile(?TAG_START, [caseless]));
-     (tag_attr)  -> element(2, re:compile(?TAG_ATTR, [caseless]));
-     (tag_end)   -> element(2, re:compile(?TAG_END, [caseless]))
+  fun(tag_begin)       -> element(2, re:compile(?TAG_BEGIN, [caseless]));
+     (tag_attr)        -> element(2, re:compile(?TAG_ATTR, [caseless]));
+     (tag_end)         -> element(2, re:compile(?TAG_END, [caseless]));
+     (tag_end_comment) -> element(2, re:compile(?COMMENT_END, [caseless]));
+     (tag_end_doctype) -> element(2, re:compile(?DOCTYPE_END, [caseless]));
+     (tag_end_xml)     -> element(2, re:compile(?XML_END, [caseless]))
   end.
 
 tokenize(State, Ix0) ->
@@ -136,43 +151,69 @@ tokenize(State, Ix0) ->
     nomatch ->
       Tail = snip(State, Ix0),
       {[{text, Tail}], eof};
-    {match, Ix1, Endp, Pre, Tag} ->
-      case tag_end(State, Ix1) of
-        {match, Ix2, Attrs, SelfClosep} ->
-          Token = {type(Endp, SelfClosep), Tag, Attrs},
+    {match, Ix1, Pre, Tag} ->
+      case tag_end(State, Ix1, Tag) of
+        {match, Ix2, Token} ->
           {[{text, Pre}, Token], Ix2};
         nomatch ->
           tokenize(State, Ix0+1)
       end
   end.
 
-type(Endp, SelfClosep) ->
-  case {Endp, SelfClosep} of
-    {false, false} -> open; %% <a>   = open
-    {true, false} -> close; %% </a>  = close
-    {false, true} -> oc;    %% <a/>  = open, close
-    {true, true} -> co      %% </a/> = close, open
-  end.
-
 tag_begin(State, Ix) ->
-  case re_run(State, tag_start, Ix) of
+  case re_run(State, tag_begin, Ix) of
     nomatch ->
       nomatch;
+    {match, [{Pos, 4}]} ->  %% comment
+      Pre = snip(State, Ix, Pos-Ix),    %% the snippet in front of the match
+      {match, Pos+4, Pre, comment};
+    {match, [{Pos, 9}]} ->  %% doctype
+      Pre = snip(State, Ix, Pos-Ix),    %% the snippet in front of the match
+      {match, Pos+9, Pre, '!'};
+    {match, [{Pos, 5}]} ->  %% xml
+      Pre = snip(State, Ix, Pos-Ix),    %% the snippet in front of the match
+      {match, Pos+5, Pre, '?'};
     {match, [{P0, _}, Endp, {Pos, Len}]} ->
       Pre = snip(State, Ix, P0-Ix),    %% the snippet in front of the match
-      Match = snip(State, Pos, Len),   %% the match
-      {match, Pos+Len, endp(Endp), Pre, Match}
+      Tag = string:lowercase(snip(State, Pos, Len)),   %% the tag
+      {match, Pos+Len, Pre, {open_close(Endp), Tag}}
   end.
 
-endp(Endp) -> {-1, 0} =/= Endp.
+open_close({-1, 0}) -> open;
+open_close(_) -> close.
 
-tag_end(State, Ix0) ->
+-define(END_NORM, [_]).
+-define(END_SELF, [_,_]).
+
+tag_end(State, Ix0, '?') ->
+  {Ix1, Attrs} = tag_attrs(State, {Ix0, []}),
+  case re_run(State, tag_end_xml, Ix1) of
+    nomatch -> nomatch;
+    {match, [{Pos, Len}]} -> {match, Pos+Len, {'?', Attrs}}
+  end;
+tag_end(State, Ix0, '!') ->
+  {Ix1, Attrs} = tag_attrs(State, {Ix0, []}),
+  case re_run(State, tag_end_doctype, Ix1) of
+    nomatch -> nomatch;
+    {match, [{Pos, Len}]} -> {match, Pos+Len, {'!', Attrs}}
+  end;
+tag_end(State, Ix0, comment) ->
+  case re_run(State, tag_end_comment, Ix0) of
+    nomatch -> {match, eof, {comment, snip(State, Ix0)}};
+    {match, [{Pos, 3}]} -> {match, Pos+3, {comment, snip(State, Ix0, Pos-Ix0)}}
+  end;
+tag_end(State, Ix0, {OpenClose, Tag}) ->
   {Ix1, Attrs} = tag_attrs(State, {Ix0, []}),
   case re_run(State, tag_end, Ix1) of
     nomatch -> nomatch;
-    {match, [_]} -> {match, Ix1+1, Attrs, false};
-    {match, [_, _]} -> {match, Ix1+2, Attrs, true}
+    {match, [{P, L}]} -> {match, P+L, {type(norm, OpenClose), Tag, Attrs}};
+    {match, [{P, L}, _]} -> {match, P+L, {type(self, OpenClose), Tag, Attrs}}
   end.
+
+type(norm, open)  -> open;  %% <a>   = open
+type(norm, close) -> close; %% </a>  = close
+type(self, open)  -> oc;    %% <a/>  = open, close
+type(self, close) -> co.    %% </a/> = close, open
 
 tag_attrs(State, {Ix, O}) ->
   case re_run(State, tag_attr, Ix) of
@@ -181,7 +222,15 @@ tag_attrs(State, {Ix, O}) ->
     {match, [_, {Pos, Len}]} ->
       tag_attrs(State, {Pos+Len, [{snip(State, Pos, Len), <<>>}|O]});
     {match, [_, {P0, L0}, {P1, L1}]} ->
-      tag_attrs(State, {P1+L1, [{snip(State, P0, L0), snip(State, P1, L1)}|O]})
+      tag_attrs(State, {P1+L1, [{snip(State, P0, L0), val(State, P1, L1)}|O]})
+  end.
+
+val(State, Pos, Len) ->
+  Snip = snip(State, Pos, Len),
+  case (-1 < (L = Len-2)) andalso Snip of
+    <<"'",X:L/binary,"'">> -> X;
+    <<"\"",X:L/binary,"\"">> -> X;
+    _ -> Snip
   end.
 
 snip(State, Ix) ->
@@ -192,142 +241,3 @@ snip(#state{subject=Bin}, Pos, Len) ->
 
 re_run(#state{subject=Subj, res=REs}, Tag, Ix) ->
   re:run(Subj, REs(Tag), [{offset, Ix}]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% tokenizer
-%%
-%% tokenize(text, Str, R) ->
-%%   case ff(text, Str, R) of
-%%     {{text, Str}, eof} ->
-%%       {[{text, Str}], eof};
-%%     {{text, Text}, Tail} ->
-%%       %% we've found a '<'. try to treat it as a tag, if that fails, s/</&lt;/
-%%       try
-%%         {Token, TailTail} = tz({tag, ""}, Tail, R),
-%%         {[{text, Text}, Token], TailTail}
-%%       catch
-%%         _:_ -> tokenize(text, <<Text/binary, "&lt;", Tail/binary>>, R)
-%%       end
-%%   end;
-%% tokenize(script, Str, R) ->
-%%   case ff(script, Str, R) of
-%%     {{script, Text}, eof} -> {[{text, Text}], eof};
-%%     {Token, Tail} -> {[Token, {close, "script"}], Tail}
-%%   end;
-%% tokenize(style, Str, R) ->
-%%   case ff(style, Str, R) of
-%%     {{style, Text}, eof} -> {[{text, Text}], eof};
-%%     {Token, Tail} -> {[Token, {close, "style"}], Tail}
-%%   end.
-%%
-%% `tz' tokenizes tags
-%% operators
-%% -define(m(X, B), <<X, B/binary>>).               % consume - match
-%% -define(d(X, B), <<X:8/integer, B/binary>>).     % consume - decompose
-%% -define(M(X, B), <<X, _/binary>> = B).           % peek - match
-%% -define(D(X, B), <<X:8/integer, _/binary>> = B). % peek - decompose
-
-%% %% tests
-%% -define(ok(X), $a=<X, X=<$z;$A=<X, X=<$Z;$0=<X, X=<$9;X==$_;X==$-;X==$:).
-%% -define(ws(X), X==$\s;X==$\r;X==$\n;X==$\t).
-%% -define(dq(X), X==$").
-%% -define(sq(X), X==$').
-%% -define(ev(X), ?ws(X);X==$>;X==$=).
-
-%% %% we have found a '<', now in tag context
-%% tz({tag, ""}, ?m("!--", _), _)          -> 'comment';
-%% tz({tag, ""}, ?m("!", Str), R)          -> '!';
-%% tz({tag, ""}, ?m("?", Str), R)          -> '?';
-%% tz({tag, ""}, ?m("/", Str), R)            -> tz({end_tag, ""}, ws(Str), R);
-%% tz({tag, Tag}, ?m("/>", Str), _)          -> {{self_closed, Tag, []}, Str};
-%% tz({tag, Tag}, ?D(X, S), R) when ?ev(X)   -> tz({attr, "", Tag, []}, ws(S), R);
-%% tz({tag, Tag}, ?d(X, Str), R) when ?ok(X) -> tz({tag, Tag++[X]}, Str, R);
-
-%% %% we've found '<!'
-%% tz({'!', DT}, ?m(">", Str), _) -> {{'!', dc(DT)}, Str};
-%% tz({'!', DT}, ?d(X, Str), R)   -> tz({'!', DT++[X]}, Str, R);
-
-%% %% we've found '<?'
-%% tz({'?', DT}, ?m("?>", Str), _) -> {{'?', dc(DT)}, Str};
-%% tz({'?', DT}, ?d(X, Str), R)    -> tz({'?', DT++[X]}, Str, R);
-
-%% %% we found '</', in end_tag context
-%% tz({end_tag, Tag, '>'}, ?m(">", Str), _)    -> {{close, dc(Tag)}, Str};
-%% tz({end_tag, Tag}, ?D(X, S), R) when ?ev(X) -> tz({end_tag, Tag, '>'}, ws(S), R);
-%% tz({end_tag, Tag}, ?d(X, Str), R)           -> tz({end_tag, Tag++[X]}, Str, R);
-
-%% %% in attribute context
-%% tz({attr, "", T, As}, ?D(X, S), R) when ?ev(X) -> tz({etag, dc(T), As}, S, R);
-%% tz({attr, "", T, As}, ?M("/>", S), R)          -> tz({etag, dc(T), As}, S, R);
-%% tz({attr, A, T, As}, ?D(X, S), R) when ?ev(X)  -> tz({eatt, dc(A), T, As}, ws(S), R);
-%% tz({attr, A, T, As}, ?M("/>", S), R)           -> tz({eatt, dc(A), T, As}, ws(S), R);
-%% tz({attr, A, T, As}, ?d(X, S), R) when ?ok(X)  -> tz({attr, A++[X], T, As}, S, R);
-
-%% %% we were in 'tag' and there should be a '>' here
-%% tz({etag, Tag, Attrs}, ?m("/>", Str), _) -> {{sc, Tag, Attrs}, Str};
-%% tz({etag, Tag, Attrs}, ?m(">", Str), _)  -> {{open, Tag, Attrs}, Str};
-
-%% %% was in 'attribute' context, found end of attribute
-%% tz({eatt, A, T, As}, ?m("=", Str), R) -> tz({val, A, T, As}, ws(Str), R);
-%% tz({eatt, A, T, As}, S, R)            -> tz({attr, "", T, As++[{A, ""}]}, ws(S), R);
-
-%% %% found an attribute that has a value
-%% tz({val, A, T, As}, ?m("'", S), R)  -> tz({sqval, "", A, T, As}, S, R); %singlequoted
-%% tz({val, A, T, As}, ?m("\"", S), R) -> tz({dqval, "", A, T, As}, S, R); %doublequoted
-%% tz({val, A, T, As}, S, R)           -> tz({uqval, "", A, T, As}, S, R); %unquoted
-
-%% %% in single quoted value context
-%% tz({sqval, V, A, T, As}, ?m("'", S), R) -> tz({attr, "", T, As++[{A, V}]}, ws(S), R);
-%% tz({sqval, V, A, T, As}, ?d(X, Str), R) -> tz({sqval, V++[X], A, T, As}, Str, R);
-
-%% %% in double quoted value context
-%% tz({dqval, V, A, T, As}, ?m("\"", S), R) -> tz({attr, "", T, As++[{A, V}]}, ws(S), R);
-%% tz({dqval, V, A, T, As}, ?d(X, Str), R)  -> tz({dqval, V++[X], A, T, As}, Str, R);
-
-%% %% in unquoted value context
-%% tz({uqval, V, A, T, As}, ?D(X, S), R) when ?ev(X) -> tz({attr, "", T, As++[{A, V}]}, ws(S), R);
-%% tz({uqval, V, A, T, As}, ?M("/>", S), R)          -> tz({attr, "", T, As++[{A, V}]}, ws(S), R);
-%% tz({uqval, V, A, T, As}, ?d(X, Str), R)           -> tz({uqval, V++[X], A, T, As}, Str, R).
-
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% %% fast forward
-
-%% %% regexps
-%% end_text() -> "<".
-%% end_script() -> "</script\\s*>".
-%% end_style() -> "</style\\s*>".
-%% end_comment() -> "-->".
-
-%% %% pre-compiled versions of the regexps
-%% mk_regexps() ->
-%%   fun(text)   -> element(2, re:compile(end_text(), [caseless]));
-%%      (script) -> element(2, re:compile(end_script(), [caseless]));
-%%      (style)  -> element(2, re:compile(end_style(), [caseless]));
-%%      (comment)-> element(2, re:compile(end_comment(), [caseless]));
-%%      (tag_start) -> element(2, re:compile(?TAG_START, [caseless]));
-%%      (tag_attr) -> element(2, re:compile(?TAG_ATTR, [caseless]));
-%%      (tag_end) -> element(2, re:compile(?TAG_END, [caseless]))
-%%   end.
-
-%% %% skip ahead until re matches. 'R' holds the pre compiled regexps
-%% ff(What, String, R) ->
-%%   case re:split(String, R(What), [{parts, 2}]) of
-%%     [Scr, Str] -> mangle(What, Scr, Str);
-%%     _ -> {{What, String}, eof}
-%%   end.
-
-%% %% the 'tz' return values
-%% mangle(text, Scr, Str)    -> {{text, Scr}, ws(Str)};
-%% mangle(script, Scr, Str)  -> {{script, Scr}, Str};
-%% mangle(style, Scr, Str)   -> {{style, Scr}, Str};
-%% mangle(comment, Scr, Str) -> {{comment, Scr}, Str}.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% utils
-
-%% trim whitespace from the left
-%% ws(?d(X, Str)) when ?ws(X) -> ws(Str);
-%% ws(Str) -> Str.
-
-%% downcase
-%% dc(Str) -> string:to_lower(Str).
